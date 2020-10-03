f7683c1101b3d8ae238e6048d7d3321e6f4a2931
rernesto <rernesto.espinosa@gmail.com>
2020-10-03 16:44:41 -0400

    Started DemoBundle development.

---
+++ src/CoralMedia/Bundle/DemoBundle/Controller/DemoDashboardController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace CoralMedia\Bundle\DemoBundle\Controller;
+
+use CoralMedia\Bundle\SecurityBundle\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DemoDashboardController extends AbstractDashboardController
+{
+    /**
+     * @Route("/admin", name="admin")
+     */
+    public function index(): Response
+    {
+        return parent::index();
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('Coral Media Boilerplate Demo');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::linkToCrud('User', 'fa fa-user', User::class);
+    }
+}
--- src/CoralMedia/Bundle/DemoBundle/DataFixtures/DemoFixtures.php
+++ src/CoralMedia/Bundle/DemoBundle/DataFixtures/DemoFixtures.php
@@ -2,6 +2,7 @@
 
 namespace CoralMedia\Bundle\DemoBundle\DataFixtures;
 
+use CoralMedia\Bundle\SecurityBundle\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,6 +10,16 @@ class DemoFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
+        $user = new User();
+        $user->setFirstName('Demo')
+            ->setLastName('User')
+            ->setEmail('demo@example.com')
+            ->setRoles(['ROLE_USER']);
+        $user->setPassword(
+            '$argon2id$v=19$m=65536,t=4,p=1$h/FMpajPUDkg1eghvp56wQ$2OTxMJCNFz7SSmbxfhy/yF7Eccqvk/OPOnhLqaStqeg'
+        );
+
+        $manager->persist($user);
 
         $manager->flush();
     }
