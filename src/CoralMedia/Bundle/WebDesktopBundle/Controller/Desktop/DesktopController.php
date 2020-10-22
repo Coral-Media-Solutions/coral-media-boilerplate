56a8f20cbbd75a9cb36841b73d4ccdc7cf0b6229
rernesto <rernesto.espinosa@gmail.com>
2020-10-22 11:09:44 -0400

    Removed unnecessary imports.

--- src/CoralMedia/Bundle/WebDesktopBundle/Controller/Desktop/DesktopController.php
+++ src/CoralMedia/Bundle/WebDesktopBundle/Controller/Desktop/DesktopController.php
@@ -2,9 +2,7 @@
 
 namespace CoralMedia\Bundle\WebDesktopBundle\Controller\Desktop;
 
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class DesktopController extends AbstractDesktopController
 {
